2e5167a60be5bdc7c350b995e58ddd94873005cb
mariano0403 <79454660+mariano0403@users.noreply.github.com>
2022-06-25 12:04:32 -0300

    Add files via upload

---
+++ Teleono/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿
+namespace Teleono
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CboCuotas = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdbCrédito = new System.Windows.Forms.RadioButton();
+            this.rdbContado = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.CboMarcas = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtImporteTotal = new System.Windows.Forms.TextBox();
+            this.txtImporteCuota = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnValorizar = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.txtVerb = new System.Windows.Forms.TextBox();
+            this.txtKingst = new System.Windows.Forms.TextBox();
+            this.txtFury = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(48, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Verbatim SSD 240Gb";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(265, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kingstom SSD 240Gb Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(492, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fury SSD 250Gb Ultra Fast";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.txtFury);
+            this.groupBox1.Controls.Add(this.txtKingst);
+            this.groupBox1.Controls.Add(this.txtVerb);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(748, 103);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Precio de disco";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.CboCuotas);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.rdbCrédito);
+            this.groupBox2.Controls.Add(this.rdbContado);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(748, 82);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Forma de pago";
+            // 
+            // CboCuotas
+            // 
+            this.CboCuotas.FormattingEnabled = true;
+            this.CboCuotas.Location = new System.Drawing.Point(450, 36);
+            this.CboCuotas.Name = "CboCuotas";
+            this.CboCuotas.Size = new System.Drawing.Size(118, 21);
+            this.CboCuotas.TabIndex = 4;
+            this.CboCuotas.SelectedIndexChanged += new System.EventHandler(this.CboCuotas_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(404, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cuotas";
+            // 
+            // rdbCrédito
+            // 
+            this.rdbCrédito.AutoSize = true;
+            this.rdbCrédito.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rdbCrédito.Location = new System.Drawing.Point(233, 40);
+            this.rdbCrédito.Name = "rdbCrédito";
+            this.rdbCrédito.Size = new System.Drawing.Size(58, 17);
+            this.rdbCrédito.TabIndex = 1;
+            this.rdbCrédito.TabStop = true;
+            this.rdbCrédito.Text = "Crédito";
+            this.rdbCrédito.UseVisualStyleBackColor = false;
+            this.rdbCrédito.CheckedChanged += new System.EventHandler(this.rdbCrédito_CheckedChanged);
+            // 
+            // rdbContado
+            // 
+            this.rdbContado.AutoSize = true;
+            this.rdbContado.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rdbContado.Location = new System.Drawing.Point(24, 40);
+            this.rdbContado.Name = "rdbContado";
+            this.rdbContado.Size = new System.Drawing.Size(65, 17);
+            this.rdbContado.TabIndex = 0;
+            this.rdbContado.TabStop = true;
+            this.rdbContado.Text = "Contado";
+            this.rdbContado.UseVisualStyleBackColor = true;
+            this.rdbContado.CheckedChanged += new System.EventHandler(this.rdbContado_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.numCantidad);
+            this.groupBox3.Controls.Add(this.btnAgregar);
+            this.groupBox3.Controls.Add(this.CboMarcas);
+            this.groupBox3.Location = new System.Drawing.Point(12, 209);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(324, 162);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Datos";
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(6, 113);
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(120, 20);
+            this.numCantidad.TabIndex = 7;
+            this.numCantidad.ValueChanged += new System.EventHandler(this.numCantidad_ValueChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAgregar.Location = new System.Drawing.Point(243, 113);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // CboMarcas
+            // 
+            this.CboMarcas.FormattingEnabled = true;
+            this.CboMarcas.Location = new System.Drawing.Point(6, 32);
+            this.CboMarcas.Name = "CboMarcas";
+            this.CboMarcas.Size = new System.Drawing.Size(118, 21);
+            this.CboMarcas.TabIndex = 5;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox4.Controls.Add(this.txtImporteTotal);
+            this.groupBox4.Controls.Add(this.txtImporteCuota);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Location = new System.Drawing.Point(419, 217);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(341, 154);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Total";
+            // 
+            // txtImporteTotal
+            // 
+            this.txtImporteTotal.Location = new System.Drawing.Point(103, 51);
+            this.txtImporteTotal.Name = "txtImporteTotal";
+            this.txtImporteTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtImporteTotal.TabIndex = 3;
+            // 
+            // txtImporteCuota
+            // 
+            this.txtImporteCuota.Location = new System.Drawing.Point(103, 102);
+            this.txtImporteCuota.Name = "txtImporteCuota";
+            this.txtImporteCuota.Size = new System.Drawing.Size(100, 20);
+            this.txtImporteCuota.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(62, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Cuota";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(62, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Importe";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnNuevo.Location = new System.Drawing.Point(36, 415);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnValorizar
+            // 
+            this.btnValorizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnValorizar.Location = new System.Drawing.Point(140, 415);
+            this.btnValorizar.Name = "btnValorizar";
+            this.btnValorizar.Size = new System.Drawing.Size(75, 23);
+            this.btnValorizar.TabIndex = 10;
+            this.btnValorizar.Text = "Valorizar";
+            this.btnValorizar.UseVisualStyleBackColor = true;
+            this.btnValorizar.Click += new System.EventHandler(this.btnValorizar_Click);
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnFinalizar.Location = new System.Drawing.Point(696, 415);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnFinalizar.TabIndex = 11;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // txtVerb
+            // 
+            this.txtVerb.Location = new System.Drawing.Point(51, 55);
+            this.txtVerb.Name = "txtVerb";
+            this.txtVerb.Size = new System.Drawing.Size(105, 20);
+            this.txtVerb.TabIndex = 3;
+            // 
+            // txtKingst
+            // 
+            this.txtKingst.Location = new System.Drawing.Point(268, 55);
+            this.txtKingst.Name = "txtKingst";
+            this.txtKingst.Size = new System.Drawing.Size(139, 20);
+            this.txtKingst.TabIndex = 4;
+            // 
+            // txtFury
+            // 
+            this.txtFury.Location = new System.Drawing.Point(495, 55);
+            this.txtFury.Name = "txtFury";
+            this.txtFury.Size = new System.Drawing.Size(132, 20);
+            this.txtFury.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.btnValorizar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox CboCuotas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rdbCrédito;
+        private System.Windows.Forms.RadioButton rdbContado;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox CboMarcas;
+        private System.Windows.Forms.TextBox txtImporteTotal;
+        private System.Windows.Forms.TextBox txtImporteCuota;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnValorizar;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.TextBox txtFury;
+        private System.Windows.Forms.TextBox txtKingst;
+        private System.Windows.Forms.TextBox txtVerb;
+    }
+}
+
---
+++ Teleono/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teleono
+{
+    public partial class Form1 : Form
+    {
+        private const float PrecioVerb = 5000;
+        private const float PrecioFury = 8000;
+        private const float PrecioKingst = 6500;
+        double Importetotal = 0;
+        int Cantidad = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CboCuotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtVerb.Text = PrecioVerb.ToString();
+            txtKingst.Text = PrecioKingst.ToString();
+            txtFury.Text = PrecioFury.ToString();
+            CboCuotas.Items.Add("1");
+            CboCuotas.Items.Add("3");
+            CboCuotas.Items.Add("6");
+            CboCuotas.Items.Add("12");
+            CboMarcas.Items.Add(label1.Text);
+            CboMarcas.Items.Add(label2.Text);
+            CboMarcas.Items.Add(label3.Text);
+            rdbContado.Checked = true;
+            rdbCrédito.Checked = false;
+            CboCuotas.Enabled = false;
+            btnAgregar.Enabled = false;
+        }
+
+        private void rdbCrédito_CheckedChanged(object sender, EventArgs e)
+        {
+            CboCuotas.Enabled = true;
+            CboCuotas.SelectedIndex = -1;
+        }
+
+        private void rdbContado_CheckedChanged(object sender, EventArgs e)
+        {
+            CboCuotas.Enabled = false;
+            
+
+        }
+
+        private void btnValorizar_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numCantidad_ValueChanged(object sender, EventArgs e)
+        {
+           
+            if (numCantidad.Value >0)
+            {
+
+                btnAgregar.Enabled = true;
+                Cantidad = int.Parse(numCantidad.Value.ToString());
+            }
+            else
+            {
+
+                btnAgregar.Enabled = false;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            switch (CboMarcas.SelectedIndex)
+            {
+                case 0:
+                    Importetotal = Importetotal + double.Parse(txtVerb.Text) * Cantidad;
+                 
+                    break;
+                case 1:
+                    Importetotal = Importetotal + double.Parse(txtKingst.Text) * Cantidad;
+                    break;
+
+                default:
+                    Importetotal = Importetotal + double.Parse(txtFury.Text) * Cantidad;
+
+                    break;
+            }
+            numCantidad.Value = 0;
+            CboMarcas.SelectedIndex = -1;
+        }
+    }
+}
+
